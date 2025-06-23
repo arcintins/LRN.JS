aa87485dd673996442a2986747e778d730bfb9b6
arcintins <budckin.i@yandex.ru>
2025-06-23 16:41:19 +0300

    Update tasks

--- P1C4/DeepSeekOther.js
+++ P1C4/DeepSeekOther.js
@@ -18,3 +18,54 @@ console.log(Object.keys(user))
 console.log(user.getPrivate())
 
 // End Task 1
+
+// Task 2
+// Дан объект person, который может быть неполным (например, отсутствует address или street). 
+// Напишите функцию getPersonStreet(obj), которая безопасно возвращает улицу пользователя или undefined, если её нет.
+
+const person = {
+    adress: {
+        street: "Lenina",
+        HouseNumber: 45. 
+    }
+}
+console.log(getPersonStreet(person))
+
+function getPersonStreet(personObj){
+    return personObj?.adress?.street
+}
+
+// End Task 2
+
+// Task 3
+// Создайте объект counter с методом valueOf, который возвращает число 5. 
+// Убедитесь, что при сложении counter + 2 получается 7.
+
+const counter = {
+    valueOf(){ return 5}
+}
+console.log(counter + 2)
+
+// End Task 3
+
+// Task 4
+// Создайте объект temperature с методами [Symbol.toPrimitive](hint), который:
+// При "number" возвращает 25 (число),
+// При "string" возвращает "25 °C",
+// При "default" возвращает "25 градусов".
+// Проверьте его преобразование в разных контекстах: сложение с числом, вывод в alert() и шаблонной строке.
+
+const temperature = {
+    temp: 500,
+    [Symbol.toPrimitive](hint){
+        if (hint === "number") return this.temp
+        if (hint === "string") return this.temp + " °C"
+        if (hint === "default") return this.temp + " градусов" // Склонений не будет
+    }
+}
+
+console.log(temperature + 5);
+console.log(Number(temperature));
+console.log(`${temperature}`);
+
+// End Task 4
