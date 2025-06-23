ffdbc902239b4484957808eccaf6cbf720587ab0
arcintins <budckin.i@yandex.ru>
2025-06-23 22:43:32 +0300

    Refactoring

--- P1C9/MentorsTasks.js
+++ P1C9/MentorsTasks.js
@@ -3,21 +3,22 @@
 // Задача от @Grand_devs
 // Write a Class ( is maybe a few ) for a linked list with no using arrays (By operation, by printing is totaly fine)
 
-class myArray {
+class MyArray {
   constructor() {
-    [this.head, this.len, this.isValue] = [null, 0, false];
+    [this.nodeEl, this.head, this.len, this.isValue, this.lastNode] = [undefined, null, 0, false, this];
   }
   
   pushEl(el) {
-    let next = this.head;
-    let current = this;
-
-    while (next !== null){
-      current = next;
-      next = next.head;
+    if (this.len === 0){
+      [this.nodeEl, this.isValue] = [el, true];
+      this.len++;
+      return
     }
 
-    [current.nodeEl, current.head, current.isValue] = [el, new myArray(), true];
+    this.lastNode.head = new MyArray();
+    this.lastNode = this.lastNode.head;
+    [this.lastNode.nodeEl, this.lastNode.isValue] = [el, true];
+
     this.len++;
   }
   
@@ -29,7 +30,7 @@ class myArray {
 
     while (counter <= index) {
       if (counter === index) {
-        if (next.isValue) {
+        if (next !== null) {
           [current.head, current.nodeEl] = [next.head, next.nodeEl];
           break;
         }
@@ -40,7 +41,8 @@ class myArray {
       next = next.head;
       counter++;
     }
-    current.len--;
+    this.lastNode = current
+    this.len--;
 
   }
 
@@ -59,10 +61,11 @@ class myArray {
   }
 }
 
-let arr = new myArray();
+let arr = new MyArray();
 arr.printList(); // []
 
 arr.pushEl(1);
+arr.printList(); // [ 1 ]
 arr.pushEl({});
 arr.pushEl(3);
 arr.printList(); // [ 1, {}, 3 ]
@@ -74,9 +77,9 @@ arr.pushEl(10);
 arr.pushEl(false);
 arr.pushEl(undefined);
 arr.pushEl("String");
-arr.printList(); // [ 1, 3, 10, false, undefined, 'Jopa' ]
+arr.printList(); // [ 1, 3, 10, false, undefined, "String" ]
 
 arr.deleteEl(5);
-arr.printList(); // [ 1, 3, 10, false, 'String' ]
+arr.printList(); // [ 1, 3, 10, false, "String" ]
 
 // End Task 1
