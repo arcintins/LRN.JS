4ea711c24c7e9e95450f8ce91c33c16a64f38fbd
arcintins <budckin.i@yandex.ru>
2025-07-07 18:28:21 +0300

    Update

--- P1C5/CodeWars_5.4-5.5.js
+++ P1C5/CodeWars_5.4-5.5.js
@@ -99,5 +99,67 @@ rotate(data, 12478) // => [3, 4, 5, 1, 2]
 // End Task 3
 
 // Task 4
+// https://www.codewars.com/kata/562e6df5cf2d3908ad00019e/javascript
+// Don't Drink the Water
+
+// Given a two-dimensional array representation of a glass of mixed liquids, 
+// sort the array such that the liquids appear in the glass based on their density. 
+// (Lower density floats to the top) The width of the glass will not change from top to bottom.
+
+// ======================
+// |   Density Chart    |
+// ======================
+// | Honey   | H | 1.36 |
+// | Water   | W | 1.00 |
+// | Alcohol | A | 0.87 |
+// | Oil     | O | 0.80 |
+// ----------------------
+
+// {                             {
+//   { 'H', 'H', 'W', 'O' },        { 'O','O','O','O' },
+//   { 'W', 'W', 'O', 'W' },  =>    { 'W','W','W','W' },
+//   { 'H', 'H', 'O', 'O' }         { 'H','H','H','H' }
+// }                             }
+ 
+// The glass representation may be larger or smaller. 
+// If a liquid doesn't fill a row, it floats to the top and to the left.
+
+function separateLiquids(glass) {
+  let glassW = glass?.[0]?.length;
+  let glassH = glass.length;
+  if (!glassW) return [];
+  let w = { H: 1, W: 2, A: 3, O: 4 };
+  let tmp = glass.flat(Infinity).sort((el1, el2) => w[el2] - w[el1]);
+  let result = [];
+  for (let i = 0; i < glassH; i++) {
+    result.push(tmp.splice(0, glassW));
+  }
+  
+  return result;
+}
+
+console.log(
+  separateLiquids([['H', 'H', 'W', 'O'],['W','W','O','W'],['H','H','O','O']])
+);
+
+console.log(
+  separateLiquids([['A','A','O','H'],['A', 'H', 'W', 'O'],['W','W','A','W'],['H','H','O','O']])
+);
+
+console.log(
+  separateLiquids([['A','H','W','O']])
+);
+
+console.log(
+  separateLiquids([['A'],['H'],['W'],['O']])
+);
 
 // End Task 4
+
+// Task 5
+
+// End Task 5
+
+// Task 6
+
+// End Task 6
